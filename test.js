b0406b7d14acd02527d46b7c58709d7e08375336
beesan-ahmad <abumfarreh@gmail.com>
2023-11-13 15:13:50 +0200

    create an instance directly

--- test.js
+++ test.js
@@ -1,5 +1,11 @@
 const Singleton = require('./Singleton');
-
+try {
+    // Trying to create an instance directly, which should throw an error
+    const directInstance = new Singleton();
+} catch (error) {
+    console.error("Caught an error: ", error.message);
+}
+//singleton instance
 try {
     const obj = Singleton.getAnInstance();//creates an instance of the privateSingleton class and logs its initial message.
     console.log(obj.message);
