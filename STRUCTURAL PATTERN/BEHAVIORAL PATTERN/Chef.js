d563848c42ea100c6c266aa8de9a6b8072d10201
beesan-ahmad <abumfarreh@gmail.com>
2023-11-16 10:40:44 +0200

    Create a Command Design Pattern in node JS(push chef as a receiver class)

---
+++ STRUCTURAL PATTERN/BEHAVIORAL PATTERN/Chef.js
@@ -0,0 +1,11 @@
+//This is the receiver class
+class Chef{
+    cookPasta(){
+        console.log('Chef is cooking Chicken Alfredo…');
+    }
+    bakeCake(){
+        console.log('Chef is baking Chocolate Cake…');
+
+    }
+} 
+module.export = Chef;
