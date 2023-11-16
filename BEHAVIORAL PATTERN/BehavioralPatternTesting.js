c52ad73becdb4c307644fa7dcb15740c96769d6c
beesan-ahmad <abumfarreh@gmail.com>
2023-11-16 12:41:57 +0200

    Create Behavioral Pattern Testing Class

---
+++ BEHAVIORAL PATTERN/BehavioralPatternTesting.js
@@ -0,0 +1,19 @@
+const Chef = require('./Chef');
+const Order = require('./Order');
+const Waiter = require('./Waiter');
+class BehavioralPatternTesting{
+    static TestingBehavioralPattern(){
+        const chef = new Chef();
+        let order = new Order(chef, 'Pasta');
+        let waiter = new Waiter(order);
+        waiter.execute();
+
+        order = new Order(chef, 'Cake');
+		waiter = new Waiter(order);
+		waiter.execute();
+
+    }
+
+
+}
+module.export = BehavioralPatternTesting;
